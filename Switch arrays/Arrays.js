bab5f48c7bb7446f8795b38366238ca9fb55edb3
gorankorac <gorankorac@gmail.com>
2022-10-25 12:40:52 +0200

    add

--- Switch arrays/Arrays.js
+++ Switch arrays/Arrays.js
@@ -3,11 +3,30 @@
 // console.log(arr[5]);
 // console.log(arr[9]);
 // console.log(arr[0]);
-
+//Zadatak 2.
 // var arr = ['Monday', 'Tusday', 'Wensday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 // console.log(arr[6]);
+// Zadatak 3.
+// var arr = [2, -4, 5, -2, -11];
 
-var arr = [2, -4, 5, -2, -11];
+// console.log(arr[1], arr[3], arr[4]);
 
-for (i=0; i<0; i++)
+// Zadatak 4.
 
+// const arr = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+// const result = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 3 === 0) {
+//     result.push(arr[i]);
+//   }
+// }
+// console.log(result);
+
+// Zadatak 5.
+var arr = [[1, 2, 1, 24], [8, 11, 9, 4],[7, 0, 7, 27],[7, 4, 28, 14,],[3, 10, 26, 7]]
+// var value = arr[0][3];
+console.log(arr[0][3]);
+
+console.log(arr[2]);
+console.log(arr[3][2]);
