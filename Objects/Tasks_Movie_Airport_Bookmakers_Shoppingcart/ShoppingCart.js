98166c16e3f6a2fbde22975120dcdefe4c6e8ef7
gorankorac <gorankorac@gmail.com>
2023-01-12 16:20:22 +0100

    Shopping Cart

--- Objects/Tasks_Movie_Airport_Bookmakers_Shoppingcart/ShoppingCart.js
+++ Objects/Tasks_Movie_Airport_Bookmakers_Shoppingcart/ShoppingCart.js
@@ -1,46 +1,113 @@
-'use strict'
+ 'use strict';
 
 (function () {
 
   function Product( n, p, ed) {
 
     this.id = Math.floor(Math.random() * 90000) + 10000;
-    // this.prodID = function () {
-
-    //     var a = (Math.Floor(Math.Random() * 10000));
-    //     return a;
-
-    // }
+   
       this.name = n;
       this.price = p;
       this.exDate = new Date(ed);
+
       this.getInfo = function () {
         var firstName = this.name[0];
         var lastName = this.name[this.name.length - 1]
-        var result = firstName.toUpperCase() + lastName.toUpperCase() + this.id + ', ' + this.name + ', ' + this.price.toFixed(2);
+        var result = firstName.toUpperCase() + lastName.toUpperCase() + this.id + ', ' + this.name + ', ' + this.price.toFixed(2) + ' $';
         return result; 
       }
-      }
+    }
+      
       var x = new Product('Banana', 15, new Date(2018, 10, 10));
       var s = new Product('Jabuka', 20, new Date(2019, 10, 10));
-       console.log(x);
-       console.log(x.getInfo());
-
-       function ShoppingBag() {
+      //  console.log(x);
+      //  console.log(x.getInfo());
+      //  console.log(s.getInfo());
+      
+  function ShoppingBag() {
 
         this.listOfProducts = [];
         this.addProduct = function(Product) {
          
-          this.listOfProducts.push(Product);
-          
+          // if (this.Product.exDate > new Date()) {
+          // } 
+           return this.listOfProducts.push(Product);
+           
         } 
+        
+
+
+        this.avgPrice = function (p) {
+
+            var sum = 0;
+            for (var i = 0; i<this.listOfProducts.length; i++) {
+              sum += this.listOfProducts[i].price
+            }
+           
+
+              return sum/this.listOfProducts.length;
+            }
+
+        this.getmostExpensive = function (p) {
+
+            var mostExpensive = this.listOfProducts[0];
+            for(var i=1; i<this.listOfProducts.length; i++) {
+              if (this.listOfProducts[i].price > mostExpensive.price) {
+                mostExpensive = this.listOfProducts[i]
+              }
+            }
+              return mostExpensive.getInfo();
+          }    
+
+        this.gettotalPrice = function (p) {
+            var total = 0;
+            for (var i=0; i<this.listOfProducts.length; i++) {
+
+              total += this.listOfProducts[i].price
+
+            }
+            return total;
+          }
+
+        }
+
+    function Paymentcard (accountbalance, active, validuntilDate) {
+
+        this.accountbalance = accountbalance;
+        this.active = active;
+        this.validuntilDate = validuntilDate;
+        this.getData = function () {
+          return this.accountbalance + ', ' + this.active + ', ' + this.validuntilDate;
+        }      
+
     }
-    var y = new ShoppingBag();
-    console.log(y);
-    console.log(y.addProduct());
+
+      function checkoutandBuy (ShoppingBag, Paymentcard) {
+        if(Paymentcard.accountbalance >= ShoppingBag.gettotalPrice()) {
+          console.log('Purchase is successful !');
+        } else {
+          console.log('the amount of' + ' ' + ((ShoppingBag.gettotalPrice())-(Paymentcard.accountbalance)) + '' + '$ is missing to complete the purchase');
+        }
+      }
 
 
+      var x = new Product('Banana', 15, new Date(2018, 10, 10));
+      var s = new Product('Jabuka', 20, new Date(2019, 10, 10));
+      
+   
+     var y = new ShoppingBag();
+     console.log(y);
 
+     y.addProduct(x)
+     y.addProduct(s)
+     console.log(y.avgPrice());
+     console.log(y.getmostExpensive());
+     console.log(y.gettotalPrice());
 
+    var card = new Paymentcard (32, 'active', new Date(2022, 15, 15))
+    console.log(card.getData());
+
+    var ende = checkoutandBuy(y, card);
+    console.log(ende);
 
   }) ();
